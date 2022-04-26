d5ff5d3e87efe20d04491daf42805955ae136de4
Rico Sandyca <ricosandyca.if@gmail.com>
2022-04-26 14:50:48 +0700

    Add vercel configuration

--- server/index.js
+++ server/index.js
@@ -4,22 +4,21 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-const port = process.env.APP_PORT;
+const port = process.env.PORT;
 const apiTarget = process.env.API_TARGET;
 
 // development purpose only
-app.use(cors());
+if (process.env.NODE_ENV === 'development') app.use(cors());
 
-// api routes
 app.use(
-  "/api",
+  '/api',
   createProxyMiddleware({
     target: apiTarget,
     changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/'
+    }
   })
 );
-
-// app routes
-app.use(express.static('../client/dist'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
