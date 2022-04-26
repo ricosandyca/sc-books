ded988c54d54625ffb2a388ac87f19439be8fb7b
Rico Sandyca <ricosandyca.if@gmail.com>
2022-04-26 13:39:49 +0700

    Define api config and types

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,6 @@
 const appConfig = {
   appName: import.meta.env.VITE_APP_NAME,
+  apiURL: import.meta.env.VITE_API_URL,
 };
 
 export default appConfig;
---
+++ src/types/book.ts
@@ -0,0 +1,15 @@
+export type BookSection = {
+  title: string;
+  content: string;
+};
+
+export type Book = {
+  id: number;
+  title: string;
+  category_id: number;
+  authors: string[];
+  cover_url: string;
+  description: string;
+  sections: BookSection[];
+  audio_length: 840;
+};
