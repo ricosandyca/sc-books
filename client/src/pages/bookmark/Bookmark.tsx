6c08babf8e0cdb71a6d5188d5638b39f928e7f3e
Rico Sandyca <ricosandyca.if@gmail.com>
2022-04-27 17:14:26 +0700

    Modify document title on bookmark page

--- client/src/pages/bookmark/Bookmark.tsx
+++ client/src/pages/bookmark/Bookmark.tsx
@@ -4,13 +4,17 @@ import { useRecoilValue } from 'recoil';
 
 import BookList from '~/components/BookList';
 import { withContainer } from '~/hoc/with-container';
+import { useDocumentTitle } from '~/hooks/use-document-title';
 import { bookmarkedListState } from '~/store/book';
 
 const Bookmark: FC = () => {
   const books = useRecoilValue(bookmarkedListState);
 
+  // change document title
+  useDocumentTitle('My Bookmark');
+
   return (
-    <VStack spacing={12} w="full" align="flex-start" minH="40vh">
+    <VStack spacing={12} w="full" align="flex-start">
       <Heading as="h2" fontSize={['xl', null, null, '2xl']}>
         My Bookmark
       </Heading>
