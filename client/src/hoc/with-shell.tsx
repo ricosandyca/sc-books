a35f97fab93bc44aac45570638e1f29cab5c9d8c
Rico Sandyca <ricosandyca.if@gmail.com>
2022-04-27 17:16:32 +0700

    Remove app footer

--- client/src/hoc/with-shell.tsx
+++ client/src/hoc/with-shell.tsx
@@ -1,6 +1,7 @@
+import { Box } from '@chakra-ui/react';
 import { FC, ComponentType } from 'react';
 
-import Footer from '~/components/Footer';
+// import Footer from '~/components/Footer';
 import Shell from '~/components/Shell';
 
 export function withShell<T>(Content: ComponentType<T>): FC<T> {
@@ -8,10 +9,12 @@ export function withShell<T>(Content: ComponentType<T>): FC<T> {
     return (
       <Shell>
         {/* Main content */}
-        <Content {...props} />
+        <Box pb={20}>
+          <Content {...props} />
+        </Box>
 
         {/* Footer */}
-        <Footer mt={[8, 12, 16, 20]} />
+        {/* <Footer mt={[8, 12, 16, 20]} /> */}
       </Shell>
     );
   };
